f943cf919452439f544f4c6e8723f8ca699edb66
RaduNichita <radunichita99@gmail.com>
2020-09-25 11:46:04 +0200

    Fixed problems

--- providers/providers.go
+++ providers/providers.go
@@ -23,33 +23,32 @@ type Provider interface {
 
 // New provides a new Provider based on the configured provider string
 func New(provider string, p *ProviderData) Provider {
-	// switch provider {
-	// case "liquid":
-	// 	return NewLiquidProvider(p)
-	// case "linkedin":
-	// 	return NewLinkedInProvider(p)
-	// case "facebook":
-	// 	return NewFacebookProvider(p)
-	// case "github":
-	// 	return NewGitHubProvider(p)
-	// case "keycloak":
-	// 	return NewKeycloakProvider(p)
-	// case "azure":
-	// 	return NewAzureProvider(p)
-	// case "gitlab":
-	// 	return NewGitLabProvider(p)
-	// case "oidc":
-	// 	return NewOIDCProvider(p)
-	// case "login.gov":
-	// 	return NewLoginGovProvider(p)
-	// case "bitbucket":
-	// 	return NewBitbucketProvider(p)
-	// case "nextcloud":
-	// 	return NewNextcloudProvider(p)
-	// case "digitalocean":
-	// 	return NewDigitalOceanProvider(p)
-	// default:
-	// 	return NewGoogleProvider(p)
-	// }
-	return NewLiquidProvider(p)
+	switch provider {
+	case "liquid":
+		return NewLiquidProvider(p)
+	case "linkedin":
+		return NewLinkedInProvider(p)
+	case "facebook":
+		return NewFacebookProvider(p)
+	case "github":
+		return NewGitHubProvider(p)
+	case "keycloak":
+		return NewKeycloakProvider(p)
+	case "azure":
+		return NewAzureProvider(p)
+	case "gitlab":
+		return NewGitLabProvider(p)
+	case "oidc":
+		return NewOIDCProvider(p)
+	case "login.gov":
+		return NewLoginGovProvider(p)
+	case "bitbucket":
+		return NewBitbucketProvider(p)
+	case "nextcloud":
+		return NewNextcloudProvider(p)
+	case "digitalocean":
+		return NewDigitalOceanProvider(p)
+	default:
+		return NewGoogleProvider(p)
+	}
 }
